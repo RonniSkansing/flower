57abd833239203bae85fd2a030ecd416a3ed2092
paula <paulajoannasobczak@gmail.com>
2015-10-17 17:45:48 +0200

    Removed Material Design + broke navigation

--- app/app.js
+++ app/app.js
@@ -1,16 +1,8 @@
 var app = angular.module('Flower', [
   'ngRoute',
-  'ngAria',
-  'ngAnimate',
-  'ngMaterial',
   'Flower.Config',
   'Flower.Encode.Converter',
   'Flower.Template',
   'Flower.CrossDomain.Iframe'
 ]);
 
-app.config(function($mdThemingProvider) {
-  $mdThemingProvider
-    .theme('default')
-    .primaryPalette('red');
-});
--- app/template/nav.js
+++ app/template/nav.js
@@ -1,23 +1,9 @@
-var app = angular.module('Flower.Template', ['ngMaterial']);
+var app = angular.module('Flower.Template', []);
 
 app.directive('navigation', function(){
   return {
-    controller: 'NavigationController',
-    controllerAs: 'navigation',
     templateUrl: '/app/template/view/nav.html'
   };
 });
 
-app.controller('NavigationController', ['$mdSidenav', function($mdSidenav){
-  var menu = {
-    xss: ''
-  };
 
-  this.getMenu = function() {
-
-  };
-
-  this.toggleSidenav = function(menuId) {
-    $mdSidenav(menuId).toggle();
-  };
-}]);
